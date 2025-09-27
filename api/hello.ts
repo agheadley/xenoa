dcee022a0a276885949e92d3cb595c35027daf30
Anthony Headley <agheadley@gmail.com>
2025-09-27 16:08:45 +0100

    a3

--- api/hello.ts
+++ api/hello.ts
@@ -2,5 +2,5 @@ export default async function handler(req, res) {
   const { body } = req;
 
   console.log(body);
-  return res.send(`Hello ${body.name}, you just parsed the request body!`);
+  return res.send(body);
 }
