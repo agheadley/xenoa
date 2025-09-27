f47be5c34711a9de4bb5b4718798dcb351e52467
Anthony Headley <agheadley@gmail.com>
2025-09-27 16:49:26 +0100

    a3

--- api/hello.ts
+++ api/hello.ts
@@ -1,9 +1,10 @@
 
-
-module.exports = (request, response) => {
-   let name = request.body.name;
-  response.status(200);
-  response.json({ message: 'It works',body:JSON.stringify(request.body.name)});
+export default {
+  fetch (request, response) {
+    let name = request.body.name;
+    response.status(200);
+    response.json({ message: 'It works',body:JSON.stringify(request.body.name)});
+  }
 };
 
 
