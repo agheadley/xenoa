311b43bf285737d3260a074e0bcd7881c2a2e149
Anthony Headley <agheadley@gmail.com>
2025-09-27 16:46:19 +0100

    a3

--- api/hello.ts
+++ api/hello.ts
@@ -1,7 +1,6 @@
 
-import { VercelRequest, VercelResponse } from "@vercel/node";
 
-module.exports = (request: VercelRequest, response: VercelResponse) => {
+module.exports = (request, response) => {
    let name = request.body.name;
   response.status(200);
   response.json({ message: 'It works',body:JSON.stringify(request.body.name)});
