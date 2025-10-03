20f5bd87bc303006a84b70a23ff9caa28deab72b
Anthony Headley <agheadley@gmail.com>
2025-10-03 14:26:44 +0100

    a8

---
+++ src/routes/+error.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+import { page } from '$app/state';
+
+</script>
+
+
+<svelte:head>
+	<title>Error</title>
+	<meta name="description" content="Implantify" />
+</svelte:head>
+
+
+<h3>Page Error</h3>
+
+<div class="card">
+  <p>{page.status} {page?.error?.message}</p>
+</div>
+<p>&nbsp;</p>
+<p><a href="/" class="button primary">Portal Home</a></p>
+
+<style>
+
+</style>
+
+
+
+
